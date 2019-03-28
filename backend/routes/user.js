783050878a65f2bdb07471b6a43f54d5f7dc2c84
kamilamin <kamil.amin@live.co.uk>
2019-03-29 00:57:50 +0500

    Server-side router for New user is updated

---
+++ backend/routes/user.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const validateRegisterInput = require("../validation/register");
+const validatorLoginInput = require("../validation/login");
+
+const User = require("../models/User");
+
+router.post("/register", function(req, res) {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "Email is already exists"
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: 200,
+        r: "pg",
+        d: "mm"
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) console.error("There was an error", err);
+        else {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) console.error("There was an Error", err);
+            else {
+              newUser.password = hash;
+              newUser.save().then(user => {
+                res.json(user);
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+});
