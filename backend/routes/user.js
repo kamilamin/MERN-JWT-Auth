75ebf3a38f2379e95cfa7eebb7b29ff1dab5df12
kamilamin <kamil.amin@live.co.uk>
2019-03-30 17:32:49 +0500

    Registeration and login Routers are added in Router.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -50,3 +50,61 @@ router.post("/register", function(req, res) {
     }
   });
 });
+
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validatorLoginInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      email = "User Not Found";
+      return res.status(400).json(errors);
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+        jwt.sign(
+          payload,
+          "secret",
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) console.error("There is some error in token", err);
+            else {
+              res.json({
+                success: true,
+                token: `Bearer ${token}`
+              });
+            }
+          }
+        );
+      } else {
+        errors.password = "Incorrent password";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+router.get(
+  "/me",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    return res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
+
+module.exports = router;
