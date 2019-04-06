2f057ecba8714374ab295b0ccff2f92d0510b364
kamilamin <kamil.amin@live.co.uk>
2019-04-06 17:28:40 +0500

    Updating Package json file and adding Navbar component to the project

---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+        <Router>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div>
+          <Link class="navbar-brand" href="#">
+            Redux Auth
+          </Link>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <Link class="nav-link" href="#">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="#">
+                Register
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="#">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      </Router>
+    );
+  }
+}
+export default Navbar;
