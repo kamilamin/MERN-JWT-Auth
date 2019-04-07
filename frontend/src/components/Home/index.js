75873b84e33066dc0ff9d53d9bb6b8d261a617fa
kamilamin <kamil.amin@live.co.uk>
2019-04-07 14:59:07 +0500

    --Commit: Home component created

---
+++ frontend/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>This is Home Component</h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
