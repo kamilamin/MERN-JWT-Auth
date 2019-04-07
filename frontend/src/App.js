b8420a3b572ca607b63b52dd16cb0a1b60c401e4
kamilamin <kamil.amin@live.co.uk>
2019-04-07 21:26:06 +0500

    --Commit: Implement Routing in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Registration";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <div class="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
