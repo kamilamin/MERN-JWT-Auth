851888ef9e4ba652803d2d52ec747fe02417e23d
kamil amin <kamil.amin@live.co.uk>
2019-06-30 10:03:15 -0700

    --Commit: save token in local storage and removing warning msgs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './setAuthToken';
+import { setCurrentUser, logoutUser} from './redux/actions/authentication'
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Registration";
-import store from "./store";
+
+if(localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = new Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login'
+  }
+}
 
 class App extends Component {
   render() {
@@ -17,9 +32,9 @@ class App extends Component {
           <div>
             <Navbar />
             <Route exact path="/" component={Home} />
-            <div class="container">
+            <div className="container">
               <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
+              <Route path="/login" component={Login} />
             </div>
           </div>
         </Router>
